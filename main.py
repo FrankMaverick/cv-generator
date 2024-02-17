5294936538c6732a01426dd08c57d8558cde02b5
francesco.cardinale <francesco.cardinale@innovaway.it>
2024-02-17 11:36:48 +0100

    create output dirs if not exist

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ import write_pdf
 import os
 import shutil
 
+def mkdir(fname):
+    try:
+        os.mkdir(fname)
+    except:
+        pass
+
 def main():
-    template_path = os.path.abspath('./templates/cv/')
-    output_html_path = os.path.abspath('./output/html/')
+    output_html_path = os.path.abspath('./output/html')
+    output_pdf_path = os.path.abspath('./output/pdf')
+    template_path = os.path.abspath('./templates/cv')
+    html_cv_path = os.path.abspath(output_html_path + '/index.html')
+    pdf_cv_path = os.path.abspath(output_pdf_path + '/cv.pdf')
+    cv_data_path = os.path.abspath('./data/cv_data.json')
+
+    # Create output dir
+    mkdir(output_html_path)
+    mkdir(output_pdf_path)
 
     # Copy the template directory path to the output path
-    shutil.copytree(template_path, output_html_path, symlinks=False, dirs_exist_ok=True)
-
-    html_cv_path = os.path.abspath(output_html_path + '/index.html')
-    cv_data_path = os.path.abspath('./data/cv_data.json')
+    shutil.copytree(template_path, output_html_path, dirs_exist_ok=True)
     
     # Populate the output HTML CV
     populate_cv.populate_html_template(html_cv_path, cv_data_path)
@@ -19,7 +30,6 @@ def main():
    
     # Create PDF in pdf_cv_path
     try:
-        pdf_cv_path = os.path.abspath('./output/pdf/cv.pdf')
         write_pdf.write_cv(html_cv_path, pdf_cv_path)
         print(f"INFO: CV saved at: {pdf_cv_path}")
     except RuntimeError as e:
